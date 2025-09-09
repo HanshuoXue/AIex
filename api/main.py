a5d76dc8aae5eb77c00e50369e89f8eece765af8
HanshuoXue <xue.hanshuo.hudson@gmail.com>
2025-09-09 18:29:09 +1200

    backend CI/CD

--- api/main.py
+++ api/main.py
@@ -1,10 +1,19 @@
+# api/main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"ok": True}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https:///red-sand-0d1794703.2.azurestaticapps.net",  # Production URL
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/healthz")
 def healthz():
-    return {"status": "ok"}
+    return {"ok": True}
